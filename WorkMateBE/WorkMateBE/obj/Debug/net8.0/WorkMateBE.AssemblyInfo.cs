57044b82f7cf6064437928111db71163cad59940
baopqd20 <baopqptit@gmail.com>
2024-12-02 22:11:50 +0700

    Add db

--- WorkMateBE/WorkMateBE/obj/Debug/net8.0/WorkMateBE.AssemblyInfo.cs
+++ WorkMateBE/WorkMateBE/obj/Debug/net8.0/WorkMateBE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkMateBE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bf03f6d4fb6b9eebb80fd92d4b7176d036da0a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9329daacb5b1e4a8ae1135f1cd0890869aa4d048")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkMateBE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkMateBE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
